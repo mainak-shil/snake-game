1d1baf7b18c291ad207193493e6f7bc02940a88a
mainak-shil <dev.mainakshil@gmail.com>
2023-09-17 12:52:10 +0530

    first commit

---
+++ App.tsx
@@ -0,0 +1,24 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
+
+import 'react-native-gesture-handler';
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
+import React from 'react';
+
+
+
+function App() {
+  return (
+    <GestureHandlerRootView style={{ flex: 1 }}>
+      {/* <Game /> */}
+      {/* <TouchableOpacity onPress={signIn}><Text>
+        Google</Text></TouchableOpacity> */}
+    </GestureHandlerRootView>
+  );
+}
+
+export default App;
